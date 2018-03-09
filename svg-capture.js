e5a6ccca13e63364ec77061d86c02c72aba4b8a5
Andrew <awong21@stuy.edu>
2018-03-08 21:43:35 -0500

    done

--- svg-capture.js
+++ svg-capture.js
@@ -1,5 +1,6 @@
 var svg = document.getElementById("svjesus");
-
+var svg_width = svg.getAttribute("width");
+var svg_height = svg.getAttribute("height");
 var b1 = document.getElementById("clear");
 
 var clear_screen = function(){
@@ -8,14 +9,10 @@ var clear_screen = function(){
     }
 };
 
-var create = function(e){
-
-    
-    
+var create_circle = function(cx, cy){
     var o = {
-
-        cx: e.offsetX,
-        cy: e.offsetY,
+        cx: cx,
+        cy: cy,
         r: 30,
         color: "black",
 
@@ -23,52 +20,32 @@ var create = function(e){
         display: function(){
             this.circle.setAttribute("cx", this.cx);
             this.circle.setAttribute("cy", this.cy);
-	    this.circle.setAttribute("r", this.r);
-   	    this.circle.setAttribute("fill", this.color);
-	    this.circle.addEventListener("click", this.changeColor);
+            this.circle.setAttribute("r", this.r);
+            this.circle.setAttribute("fill", this.color);
+            this.circle.addEventListener("click", this.changeColor, true);
             svg.appendChild(this.circle);
         },
 
-	changeColor: function(){
-            
-            newColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-            //this.circle.setAttribute("fill", newColor);
-	    this.color = newColor;
-	    console.log(this.color);
-	},
+        changeColor: function(e){
+            e.stopPropagation();
+            hex = "0123456789ABCDEF";
+            newColor = "#";
+            for (var i = 0; i < 6; i++){
+                newColor = newColor + hex[Math.floor(Math.random() * 16)];
+            };
+            o.circle.setAttribute("fill", newColor);
+            o.color = newColor;
+            o.circle.addEventListener("click", o.remove, true);
+        },
 
-        remove: function(){
-            svg.removeChild(this.circle);
+        remove: function(e){
+            svg.removeChild(o.circle);
+            create_circle(Math.floor(Math.random() * svg_width), Math.floor(Math.random() * svg_height));
         }
     };
     o.display();
-    console.log(o);
     return o;
 };
 
-var make_circle = function(e){
-    circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("cx", e.offsetX);
-    circle.setAttribute("cy", e.offsetY);
-    circle.setAttribute("r", "30");
-    circle.setAttribute("fill", "black");
-    circle.setAttribute("stroke", "black");
-    svg.appendChild(circle);
-    circle.addEventListener("click", change_color, true);
-};
-
-var delete_circle = function(e){
-    svg.removeChild(this);
-    circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("cx", Math.random()*500);
-    circle.setAttribute("cy", Math.random()*500);
-    circle.setAttribute("r", "30");
-    circle.setAttribute("fill", "black");
-    circle.setAttribute("stroke", "black");
-    svg.appendChild(circle);
-}
-
-
-
 b1.addEventListener("click", clear_screen);
-svg.addEventListener("click", create);
+svg.addEventListener("click", function(e) { create_circle(e.offsetX, e.offsetY); } );
